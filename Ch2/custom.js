f72930a4c7e5f8ca8bbc474b93fbd61a34c4b022
mrmezan06 <mrmezan06@gmail.com>
2021-11-24 10:57:13 +0600

    Variable declaration

--- Ch2/custom.js
+++ Ch2/custom.js
@@ -1,1 +1,15 @@
-alert("Hello world");
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const point = 100;
+// point = 50;
+// const variable couldn't change
+console.log(point);
+
+var score = 200;
+console.log(score)
